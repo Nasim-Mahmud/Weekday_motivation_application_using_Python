2994fbf9abff45e74acbaf51fa5bc275bb84d918
Nasim Mahmud <nasim.mishu@northsouth.edu>
2022-06-17 10:41:59 +0600

    Initializing and testing.

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 from smtplib import *
 import datetime as dt
+import random
 
+# Finding the day of the week.
 today = dt.datetime.now()
 current_weekday = today.weekday()
 
+
+# Reading the quote file.
 with open("quotes.txt", "r") as data:
-    quotes = data.readlines()
-    print(quotes)
+    all_quotes = data.readlines()
+    quote = random.choice(all_quotes)
+    print(quote)
 
