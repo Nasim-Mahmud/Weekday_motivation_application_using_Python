5b45a6152e50d6bde2f252a16b056f59b82a997d
Nasim Mahmud <nasim.mishu@northsouth.edu>
2022-06-17 10:37:12 +0600

    Initializing and testing.

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from smtplib import *
 import datetime as dt
+import pandas
 
 today = dt.datetime.now()
 current_weekday = today.weekday()
+
+with open("quotes.txt", "r") as data:
+    quotes = data.readlines()
+    print(quotes)
 
